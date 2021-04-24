fef925dee3a6db4b289e6d503459513e0ef15748
cola2 <62168775+cola2@users.noreply.github.com>
2021-04-24 15:21:04 +0800

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,8 +1,40 @@
-import re
-gmail = input("輸入你的gmail")
+import requests
+from bs4 import BeautifulSoup
 
-if re.fullmatch("\w+@gmail.com$", gmail) != None:
-    print('對了!')
+
+
+
+postData = {'_csrf': 'b48de1d4-8f7b-4777-847a-29017f2d153a',
+'startStation': '1000-臺北',
+'endStation': '4400-高雄',
+'transfer': 'ONE',
+'rideDate': '2021/04/24',
+'startOrEndTime': 'true',
+'startTime': '07:00',
+'endTime': '08:00',
+'trainTypeList': 'ALL',
+'_isQryEarlyBirdTrn': 'on',
+'query': '查詢'}
+
+respond = requests.post('https://www.railway.gov.tw/tra-tip-web/tip/tip001/tip112/querybytime', data = postData)
+soup = BeautifulSoup(respond.text, 'html.parser')
+
+data = soup.find_all('tr', class_ = 'trip-column')
+
+for info in data:
     
-else:
-    print("錯了motherfucker")
+    location = info.find_all('span', class_='location')
+    location1 = location[0].text
+    location2 = location[1].text
+    
+    time = info.find_all('td')
+    time1 = time[1].text
+    time2 = time[2].text
+    time3 = time[3].text
+    time4 = time[4].text
+    
+    print(location1, '--->', location2, time1, time2, time3, time4)
+    
+    
+
+
